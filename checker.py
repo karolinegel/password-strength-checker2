2912e277ecaa594ae0fcf96e39094524bbb59c25
Karoline Geleisha <154263960+karolinegel@users.noreply.github.com>
2026-02-07 02:08:30 +0300

    add password input handling

--- checker.py
+++ checker.py
@@ -6,3 +6,6 @@ def is_strong(password):
     if password.isalnum():
         return False
     return True
+if __name__ == "__main__":
+    pwd = input("Enter password: ")
+    print("Strong" if is_strong(pwd) else "Weak")
